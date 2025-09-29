4810300210ca7eb056f96926e170bd9a98997752
Marcos Longo <marcoslongo@pecege.com>
2025-09-29 11:32:59 -0300

    wip

--- src/components/layout/home/Avaliation/index.tsx
+++ src/components/layout/home/Avaliation/index.tsx
@@ -4,12 +4,12 @@ export function Avaliation() {
   return (
     <section className="bg-[#483b35] pt-24 pb-20">
       <div className="container mx-auto">
-        <div className="flex justify-between">
-          <div className="text-white max-w-[33%] flex flex-col gap-2">
+        <div className="flex justify-between flex-wrap">
+          <div className="text-white w-[33%] flex flex-col gap-2">
             <h2 className="text-4xl font-bold">O <span className="text-[#9a8167]">MELHOR</span> <br /> <span className="text-[#9a8167]">ATENDIMENTO</span> E <span className="text-[#9a8167]">PROFISSIONALISMO</span></h2>
             <p className="text-lg">Somos especialistas em negócios imobiliários</p>
           </div>
-          <div>
+          <div className="w-[66%]">
             <div className="grid grid-cols-2 gap-5">
               <div className="flex flex-col justify-center bg-white rounded-2xl p-4 pb-8">
                 <div className="flex justify-center mt-[-70px]">
@@ -40,7 +40,7 @@ export function Avaliation() {
                 </div>
                 <div className="text-center flex flex-col gap-1">
                   <h3 className="font-bold text-2xl text-[#483b35]">Avaliamos seu imóvel</h3>
-                  <p className="text-md text-[#9a8167] font-semibold">Cadastre seu imóvel que em breve entraremos em contato.</p>
+                  <p className="text-md text-[#9a8167] font-semibold">Cadastre seu imóvel que em breve <br /> entraremos em contato.</p>
                 </div>
               </div>
             </div>
